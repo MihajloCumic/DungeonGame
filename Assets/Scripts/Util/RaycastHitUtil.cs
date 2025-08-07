fe1499086f3f89e7e3f9bde4d93c7efa4f1f311e
MihajloCumic <mihajlo.cumic1999@gmail.com>
2025-08-07 22:22:38 +0200

    Added Player layer

---
+++ Assets/Scripts/Util/RaycastHitUtil.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RaycastHitUtil
+{
+    public static bool IncludePlayerLayer(Ray ray, out RaycastHit hit, float distance)
+    {
+        return Physics.Raycast(ray, out hit, distance);
+    }
+    public static bool ExludePlayerLayer(Ray ray, out RaycastHit hit, float distance)
+    {
+        int layer = LayerMask.NameToLayer("PlayerLayer");
+        LayerMask exludePlayer = ~(1 << layer);
+        return Physics.Raycast(ray, out hit, distance, exludePlayer);
+    }
+}
