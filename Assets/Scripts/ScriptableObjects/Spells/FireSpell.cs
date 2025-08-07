ebc0b17a738aea3b70e8779f3e3c622af27e0e4f
MihajloCumic <mihajlo.cumic1999@gmail.com>
2025-08-07 20:14:53 +0200

    Indicator fixed

--- Assets/Scripts/ScriptableObjects/Spells/FireSpell.cs
+++ Assets/Scripts/ScriptableObjects/Spells/FireSpell.cs
@@ -10,5 +10,22 @@ public class FireSpell : Spell
     public float MaxDistance => _maxDistance;
     public float BurnRadius => _burnRadius;
     public GameObject Indicator => _indicator;
+
+
+    public void DrawIndicator(Vector3 playerPosition, Ray mouseRay, GameObject indicator)
+    {
+        if (Physics.Raycast(mouseRay, out RaycastHit hit))
+        {
+            var distance = Vector3.Distance(playerPosition, hit.point);
+            var oldScale = indicator.transform.localScale;
+            indicator.transform.localScale = new Vector3(oldScale.x, distance, oldScale.z);
+            indicator.transform.position = (playerPosition + hit.point) / 2f;
+
+            Vector3 direction = hit.point - playerPosition;
+            indicator.transform.rotation = Quaternion.LookRotation(direction);
+            indicator.transform.Rotate(90, 0, 0);
+        }
+        Debug.Log(indicator.activeInHierarchy);
+    }
     
 }
--- Assets/Scripts/SpellCaster.cs
+++ Assets/Scripts/SpellCaster.cs
@@ -5,9 +5,7 @@ public class SpellCaster : MonoBehaviour
 
     private ICommand _command;
     [SerializeField] private FireSpell firespellSo;
-    [SerializeField] private GameObject marker;
     private GameObject _indicator;
-    private const float maxSize = 5f;
 
     void Awake()
     {
@@ -19,45 +17,23 @@ public class SpellCaster : MonoBehaviour
         _indicator = Instantiate(firespellSo.Indicator);
         _indicator.SetActive(false);
     }
+
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.Q))
         {
-            
-        }
-        if (Input.GetKey(KeyCode.Q) && Input.GetMouseButton(0))
+            if (Input.GetMouseButton(0))
             {
-                _indicator.transform.position = transform.position;
-
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, out RaycastHit hit))
-                {
-
-                    var childsTransform = _indicator.GetComponentInChildren<Transform>();
-                    Vector3 direction = hit.point - childsTransform.transform.position;
-                    // var m = Instantiate(marker);
-                    // m.transform.position = hit.point;
-                    // return;
-                    direction.y = 0;
-                    Quaternion targetRotation = Quaternion.LookRotation(direction);
-                    _indicator.transform.rotation = targetRotation;
-
-                    //Scale
-                    var tmpDistance = Vector3.Distance(_indicator.transform.position, hit.point);
-                    var distance = tmpDistance > 5f ? tmpDistance : tmpDistance;
-
-                    float scaleZ = distance / 10f;
-                    var scaleX = childsTransform.localScale.x;
-                    childsTransform.localScale = new Vector3(scaleX, 1f, scaleZ);
-                }
                 _indicator.SetActive(true);
-                return;
+                var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                firespellSo.DrawIndicator(transform.position, mouseRay, _indicator);
             }
-        if (Input.GetKey(KeyCode.Q) && Input.GetMouseButtonUp(0))
-        {
-            _command.Execute();
-        }
+            if (Input.GetMouseButtonUp(0))
+            {
+                _command.Execute();
+            }
+            return;
+        }       
         _indicator.SetActive(false);
     }
 }
